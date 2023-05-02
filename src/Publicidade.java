ba4c9acd06dd828dce129aff392c8afd8d8ce3d2
Ramon Alves Gomes <12114553@aluno.newtonpaiva.br>
2023-05-02 20:58:29 -0300

    Initial commit

---
+++ src/Publicidade.java
@@ -0,0 +1,22 @@
+public class Publicidade {
+    private  String dataPublicacao,textoPublicacao,
+
+    public Publicidade(String dataPublicacao, String textoPublicacao) {
+        this.dataPublicacao = dataPublicacao;
+        this.textoPublicacao = textoPublicacao;
+    }
+}
+
+
+public Publicacao(String dataPublicacao, String textoPublicacao, String)
+
+    public Publicidade(String dataPublicacao) {
+        this.dataPublicacao = dataPublicacao;
+    }
+    this.dataPublicacao=d dataPublicacao;
+    this.textoPublicacao=textoPublicacao;
+    this.linkMidia;
+
+
+
+
---
+++ src/RedeSocial.java
@@ -0,0 +1,3 @@
+public class RedeSocial {
+}
+private  String dataCriacao
